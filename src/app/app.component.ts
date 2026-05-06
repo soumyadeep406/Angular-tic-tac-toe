19bbe2f046bbd9e9b787444c6811e108ae7fbabd
soumyadeep406 <bhattacharyasoumyadeep462@gmail.com>
2026-05-06 20:54:57 +0530

    Added Angular Tic Tac Toe project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TicTacToeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tic-tac-toe';
 }
